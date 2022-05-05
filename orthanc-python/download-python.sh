c26846f1807890f39f51801be33e0756cec02dc0
Pauline <4224001+paulineribeyre@users.noreply.github.com>
2022-05-05 16:48:40 -0500

    Revert "python to 3.9"
    This reverts commit 253b8faf72972aeeb70e8ea8b94df748a572c041.

--- orthanc-python/download-python.sh
+++ orthanc-python/download-python.sh
@@ -4,8 +4,8 @@ set -e
 cd
 
 URL=https://lsb.orthanc-server.com/
-VERSION=debian-bullseye-python-3.9/mainline
+VERSION=mainline
 
-wget ${URL}/plugin-python/${VERSION}/libOrthancPython.so
+wget ${URL}/plugin-python/debian-buster-python-3.7/${VERSION}/libOrthancPython.so
 
 mv ./libOrthancPython.so  /usr/local/share/orthanc/plugins/
